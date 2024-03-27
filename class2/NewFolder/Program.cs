885610893363ca5d2145fa788a43fdcb703fd201
Aliyev-Rufat <Rufat0747@gmail.com>
2024-03-27 23:39:22 +0400

    class task2

---
+++ class2/NewFolder/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+static void Main(string[] args)
+{
+    Human human = new Human("Rufat", "Aliyev", "Male", 20);
+    human.ShowInfo();
+
+    Doctor doctor = new Doctor("Leyla", "Ferecli", "Female", 20, "dentist", 13);
+    doctor.ShowInfo();
+}
